b340bd4553043b375e16d1571d4b85f1e4c992f3
clover3 <lesterny@gmail.com>
2018-11-11 00:52:33 -0500

    fixes #3,#4

---
+++ src/models/transformer/layer.py
@@ -0,0 +1,9 @@
+from __future__ import print_function
+import tensorflow as tf
+
+from models.transformer.hyperparams import Hyperparams as hp
+from models.transformer.modules import *
+import os, codecs
+from tqdm import tqdm
+
+
---
+++ src/task/MLM.py
@@ -0,0 +1,26 @@
+
+from models.transformer.transformer import *
+
+
+class TransformerLM:
+    def __init__(self, voca_size, is_training):
+        # define decoder inputs
+        self.x = tf.placeholder(dtype=tf.int32, shape=[None, hp.seq_max])
+        self.y = tf.placeholder(tf.int32, shape=(None, hp.seq_max))
+
+        self.enc = transformer_encode(self.x, voca_size, is_training)
+        # Decoder
+
+        # Final linear projection
+        self.logits = tf.layers.dense(self.enc, voca_size)
+        self.preds = tf.to_int32(tf.arg_max(self.logits, dimension=-1))
+        self.istarget = tf.to_float(tf.not_equal(self.y, 0))
+        self.acc = tf.reduce_sum(tf.to_float(tf.equal(self.preds, self.y)) * self.istarget) / (tf.reduce_sum(self.istarget))
+        tf.summary.scalar('acc', self.acc)
+
+        if is_training:
+            # Loss
+            self.y_smoothed = label_smoothing(tf.one_hot(self.y, depth=voca_size))
+            self.loss = tf.nn.softmax_cross_entropy_with_logits(logits=self.logits, labels=self.y_smoothed)
+            self.mean_loss = tf.reduce_sum(self.loss * self.istarget) / (tf.reduce_sum(self.istarget))
+            tf.summary.scalar('mean_loss', self.mean_loss)
