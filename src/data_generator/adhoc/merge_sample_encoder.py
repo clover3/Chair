0ceeadf71af959e008d71529fab744e44dcf3fd7
clover3 <lesterny@gmail.com>
2019-01-27 19:13:42 -0500

    Merger Sampler

--- src/data_generator/adhoc/merge_sample_encoder.py
+++ src/data_generator/adhoc/merge_sample_encoder.py
@@ -5,7 +5,7 @@ import xmlrpc.client
 from data_generator.data_parser.trec import load_trec, load_robust
 import random
 import csv
-from path import data_path
+from path import data_path, output_path
 from data_generator.tokenizer_b import EncoderUnit
 from concurrent.futures import ProcessPoolExecutor, wait, ThreadPoolExecutor
 from trainer.promise import PromiseKeeper, MyPromise, list_future
@@ -21,6 +21,83 @@ def load_marco_query(path):
         query = row[1]
         queries[q_id] = query
     return queries
+
+def split_text(content):
+    idx = 0
+    window_size = 200 * 3
+    while idx < len(content):
+        span = content[idx:idx + window_size]
+        yield span
+        idx += window_size
+
+
+def pickle_payload(slave_id):
+    query_path = "/mnt/nfs/work3/youngwookim/code/adhoc/robus/queries.train.tsv"
+    docs = load_robust("/mnt/nfs/work3/youngwookim/data/robust04")
+    query = load_marco_query(query_path)
+
+    def split_encoder(l):
+        step = 200
+        idx = 0
+        r = []
+        while idx < len(l):
+            r += slave_fn(l[idx:idx+step])
+            idx += step
+            if idx % 1000 == 200:
+                print(idx)
+        return r
+
+    print("{}] Load sampled".format(slave_id))
+    dir_path = "/mnt/nfs/work3/youngwookim/code/adhoc/robus/marco_query_doc_results"
+
+    sample_filename = os.path.join(dir_path, "merger_train_{}.pickle".format(slave_id))
+    sampled = pickle.load(open(sample_filename, "rb"))
+
+    inst = []
+    for q_id_1, doc_id_1, q_id_2, doc_id_2 in sampled:
+        q1 = query[q_id_1]
+        runs_1 = []
+        for span in split_text(docs[doc_id_1]):
+            runs_1.append((q1, span))
+
+        q2 = query[q_id_2]
+        runs_2 = []
+        for span in split_text(docs[doc_id_2]):
+            runs_2.append((q2, span))
+
+        inst.append((runs_1, runs_2))
+    print(len(inst))
+    for i in range(10):
+        st = i * 1000
+        ed = (i+1) * 1000
+        name = str(slave_id) + str(i)
+        pickle.dump(inst[st:ed], open("../output/doc_pair_{}.pickle".format(name), "wb"))
+
+
+def encode_payload(idx):
+    doc_pairs = pickle.load(open("../output/doc_pair_{}.pickle".format(idx), "rb"))
+    vocab_filename = "bert_voca.txt"
+    voca_path = os.path.join(data_path, vocab_filename)
+    max_sequence = 200
+    encoder_unit = EncoderUnit(max_sequence, voca_path)
+
+    
+    result = []
+    for runs_1, runs_2 in doc_pairs[idx:]:
+        enc_run_1 = [] 
+        for query, text, in runs_1:
+            entry = encoder_unit.encode_pair(query, text)
+            enc_run_1.append((entry["input_ids"], entry["input_mask"], entry["segment_ids"]))
+
+        enc_run_2 = [] 
+        for query, text, in runs_2:
+            entry = encoder_unit.encode_pair(query, text)
+            enc_run_2.append((entry["input_ids"], entry["input_mask"], entry["segment_ids"]))
+        result.append((enc_run_1, enc_run_2))
+
+    filename = os.path.join(output_path, "dp_train_{}.pickle".format(idx))
+    pickle.dump(result, open(filename, "wb"))
+
 
 class MasterEncoder:
     def __init__(self, query_path, doc_id_path):
@@ -157,5 +234,10 @@ def start_master():
 if __name__ == '__main__':
     if sys.argv[1] == "slave":
         start_slave()
+    elif sys.argv[1] == "pickle":
+        pickle_payload(int(sys.argv[2]))
+    elif sys.argv[1] == "encode":
+        encode_payload(int(sys.argv[2]))
     else:
         start_master()
+
