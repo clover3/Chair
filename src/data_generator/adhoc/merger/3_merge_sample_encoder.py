0a5961b8128d35db0dda4dcf1b87590163436209
clover3 <lesterny@gmail.com>
2019-02-04 22:31:39 -0500

    merger fix

--- src/data_generator/adhoc/merger/3_merge_sample_encoder.py
+++ src/data_generator/adhoc/merger/3_merge_sample_encoder.py
@@ -13,23 +13,32 @@ import pickle
 import sys
 
 
+def split_text(content):
+    idx = 0
+    window_size = 200 * 3
+    while idx < len(content):
+        span = content[idx:idx + window_size]
+        yield span
+        idx += window_size
+
+
 def encode_payload(idx):
-    doc_pairs = pickle.load(open("../output/merger_plainpair_{}.pickle".format(idx), "rb"))
+    doc_pairs = pickle.load(open("../output/merger_plainpair_{0:02d}.pickle".format(idx), "rb"))
     vocab_filename = "bert_voca.txt"
     voca_path = os.path.join(data_path, vocab_filename)
     max_sequence = 200
     encoder_unit = EncoderUnit(max_sequence, voca_path)
 
     result = []
-    for runs_1, runs_2 in doc_pairs:
+    for q1, d1, q2, d2 in doc_pairs:
         enc_run_1 = [] 
-        for query, text, in runs_1:
-            entry = encoder_unit.encode_pair(query, text)
+        for text in split_text(d1):
+            entry = encoder_unit.encode_pair(q1, text)
             enc_run_1.append((entry["input_ids"], entry["input_mask"], entry["segment_ids"]))
 
-        enc_run_2 = [] 
-        for query, text, in runs_2:
-            entry = encoder_unit.encode_pair(query, text)
+        enc_run_2 = []
+        for text in split_text(d2):
+            entry = encoder_unit.encode_pair(q2, text)
             enc_run_2.append((entry["input_ids"], entry["input_mask"], entry["segment_ids"]))
         result.append((enc_run_1, enc_run_2))
 
