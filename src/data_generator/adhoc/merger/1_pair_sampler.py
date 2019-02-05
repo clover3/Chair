2a0d0932f23498a4f61e36c9d3b4452145866f59
clover3 <lesterny@gmail.com>
2019-02-04 21:47:27 -0500

    Only compare among same query

--- src/data_generator/adhoc/merger/1_pair_sampler.py
+++ src/data_generator/adhoc/merger/1_pair_sampler.py
@@ -1,27 +1,35 @@
 import random
 import pickle
+import os
+import sys
 
+from config.input_path import galago_judge_path
+from collections import defaultdict
 ## read data
 
 def read_list(i):
     st = i * 10000
     ed = (i+1) * 10000
-    f = open("{}_{}.list".format(st, ed), "r")
+    dir_path = galago_judge_path
+    file_path = os.path.join(dir_path, "{}_{}.list".format(st, ed))
+    f = open(file_path, "r")
+    group_by_q_id = defaultdict(list)
     for line in f:
         q_id, _, doc_id, rank, score, _ = line.split()
-        yield q_id, doc_id, score
+        group_by_q_id[q_id].append((doc_id, score))
 
+    return group_by_q_id
 
-## debais sampling
+## debias sampling
 def sample_debias(ranked_list):
-    max_occurence = 10000
+    max_occurence = 100
     output = {}
-    for q_id, doc_id, score in ranked_list:
+    for doc_id, score in ranked_list:
         score_grouper = int(float(score) + 0.8)
         if score_grouper not in output:
             output[score_grouper] = []
         if len(output[score_grouper]) < max_occurence:
-            output[score_grouper].append((q_id, doc_id, score))
+            output[score_grouper].append((doc_id, score))
     return output
 
 
@@ -29,12 +37,14 @@ def pick1(l):
     return l[random.randrange(len(l))]
 
 
-def generate_data():
-    print("reading data")
-    big_i = 0
-    for big_i in range(1,10):
-        l = read_list(big_i)
+def generate_data(run_id):
+    group_by_q_id = read_list(run_id)
+    result = []
+    for q_id in group_by_q_id:
+        l = group_by_q_id[q_id]
         groups = sample_debias(l)
+        if max(list(groups.keys())) < 15:
+            continue
 
         def sample_one(keys, d):
             key = pick1(keys)
@@ -42,22 +52,20 @@ def generate_data():
             return entry
 
         keys = list(groups.keys())
-        data_size = 10 * 1000
+        data_size = 50
 
         print("Sampling")
-        for j in range(10):
-            result = []
-            for i in range(data_size):
-                q_id_1, doc_id_1, score1 = sample_one(keys, groups)
-                q_id_2, doc_id_2, score2 = sample_one(keys, groups)
+        for i in range(data_size):
+            doc_id_1, score1 = sample_one(keys, groups)
+            doc_id_2, score2 = sample_one(keys, groups)
 
-                if score1 < score2 :
-                    result.append((q_id_1, doc_id_1, q_id_2, doc_id_2))
-                else:
-                    result.append((q_id_2, doc_id_2, q_id_1, doc_id_1))
-            gid = big_i * 10 + j
-            pickle.dump(result, open("merger_idpair_{}.pickle".format(gid), "wb"))
+            if score1 < score2 :
+                result.append((q_id, doc_id_1, q_id, doc_id_2))
+            else:
+                result.append((q_id, doc_id_2, q_id, doc_id_1))
+    pickle.dump(result, open("merger_idpair_{}.pickle".format(run_id), "wb"))
 
 
 if __name__ == "__main__":
-    generate_data()
+    run_id = int(sys.argv[1])
+    generate_data(run_id)
