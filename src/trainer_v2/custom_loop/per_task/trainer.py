change loss calculation - Bug: When there is multiple worker, loss is not averaged.

src/trainer_v2/custom_loop/per_task/trainer.py
@@ -24,7 +24,6 @@ class Trainer(TrainerIF):
         self.inner_model = inner_model
 
         # These variables will be initialized by build_model()
-        self.training_loss = None
         self.train_metrics = None
         self.keras_model = None
         self.optimizer = None
@@ -38,8 +37,7 @@ class Trainer(TrainerIF):
             self.keras_model.summary(140)
             optimizer = AdamWeightDecay(learning_rate=run_config.train_config.learning_rate)
             self.keras_model.optimizer = optimizer
-            self.training_loss = tf.keras.metrics.Mean('training_loss', dtype=tf.float32)
-            self.train_metrics = {'loss': self.training_loss}
+            self.train_metrics = {}
             self.optimizer = optimizer
         else:
             pass
@@ -69,7 +67,6 @@ class Trainer(TrainerIF):
 
         gradients = tape.gradient(loss, model.trainable_variables)
         apply_gradient_warning_less(self.optimizer, gradients, model.trainable_variables)
-        self.training_loss.update_state(loss)
         return loss
 
     def get_train_metrics(self) -> Dict[str, tf.keras.metrics.Metric]:

src/trainer_v2/custom_loop/train_loop.py
@@ -124,9 +124,18 @@ def tf_run_train(run_config: RunConfig2,
         @tf.function
         def distributed_train_step(train_itr):
             # try:
+            total_loss = 0.0
+            n_step = 0.
             for _ in tf.range(run_config.common_run_config.steps_per_execution):
                 batch_item = next(train_itr)
                 per_replica_losses = strategy.run(trainer.train_step, args=(batch_item, ))
+                loss = strategy.reduce(
+                    tf.distribute.ReduceOp.SUM, per_replica_losses, axis=None)
+                total_loss += loss
+                n_step += 1.
+
+            train_loss = total_loss / n_step
+            return train_loss
             # except StopIteration as e:
             #     print(e)
 
@@ -148,9 +157,10 @@ def tf_run_train(run_config: RunConfig2,
             metrics = trainer.get_train_metrics()
             for m in metrics.values():
                 m.reset_state()
-            distributed_train_step(train_itr)
+            train_loss = distributed_train_step(train_itr)
             trainer.train_callback()
             msg = summarize_metric(fetch_metric_result(metrics))
+            per_step_msg += " loss_b={0:.6f} ".format(train_loss)
             per_step_msg += msg
 
             if f_do_eval: